6847cfc41a6046d1b73dc1f256dcd0313a4028f4
No-Mamun <94072559+No-Mamun@users.noreply.github.com>
2023-01-21 15:10:09 +0600

    Add files via upload
    Data types & Structures

---
+++ main.js
@@ -0,0 +1,148 @@
+
+/*
+Data Type, boolean;
+true and false;
+let name_1 = 10;
+let name_2 = 20;
+
+let Mamun = name_1 == name_2;
+
+console.log(Mamun);
+
+object:1;
+const name ={
+              name_1:"Mamun",
+              name_2:"Khan",
+              name_3:"Babu",
+}
+console.log(name);
+
+*/
+
+/*
+object:2;
+
+let carinfo = {
+              name:"Allion",
+
+              brand:"Toyta",
+
+              model: 2020,
+
+              price:400000,
+};
+
+console.log(carinfo["price"]);
+*/
+
+/*
+
+*/
+/*
+object:3;
+
+let carInfo = {};
+
+carInfo[name]= "Allion";
+
+console.log(carInfo[name]);
+
+*/
+
+/*
+Function;
+
+function saHai() {
+
+              let name = "Mamun";
+
+              let msg = "Hi We are Learning jsa";
+
+              alert(msg);
+}
+saHai();
+
+*/
+
+
+
+/*
+Data Type
+Template string:1,
+
+const name = "mamun";
+
+const age = 35;
+
+console.log(`My name is ${name} and i am ${age} `);
+*/
+
+/*
+Template string:2,
+
+const name = "mamun";
+
+const age = 35;
+
+const hello =`My name is ${name} and i am ${age}`;
+
+console.log(hello);
+*/
+
+/*
+Data Type:3;
+console data type,propati and mathord;
+propati,
+const s =("Hello World!");
+
+console.log(s.length);
+
+*/
+/*
+Data Type:4;
+console data type,propati and mathord;
+mathord:1,
+
+const s =("Hello World!");
+
+console.log(s.toUpperCase());
+*/
+/*
+Data Type:5;
+console data type,propati and mathord;
+mathord:2,
+
+const s =("Hello World!");
+
+console.log(s.substring(0,5));
+*/
+
+/*
+Data Type:6;
+console data type,propati and mathord;
+mathord:3,
+
+const s =("Hello World!");
+
+console.log(s.substring(5,12));
+*/
+
+/*
+Data Type:7;
+console data type,propati and mathord;
+mathord:4,
+
+const s =("Hello World!");
+
+console.log(s.substring(5,12).toUpperCase());
+*/
+
+/*
+Data Type:8;
+console data type,propati and mathord;
+mathord:5,
+
+const s =("Hello World!");
+
+console.log(s.split(''));
+*/
